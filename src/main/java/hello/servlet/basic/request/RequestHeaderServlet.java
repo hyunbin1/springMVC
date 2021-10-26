67afcf9e367c7085b69446d2b73ec76307564388
hyunbin1 <kimgusqls1@gmail.com>
2021-10-26 10:52:56 +0900

    21/10/26 get 방식 파라미터 데이터 얻는 방법

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -14,6 +14,7 @@ public class RequestHeaderServlet extends HttpServlet {
     @Override // Ctrl+o - override - service 잠겨있는거 선택하기
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         printStartLine(request);
+        printHeaders(request);
     }
 
 
@@ -23,7 +24,7 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getProtocol() = " + request.getProtocol()); // HTTP/1.1
         System.out.println("request.getScheme() = " + request.getScheme()); // http
 
-        // http://localhost:8080request-header
+        // http://localhost:8080request-header - 전체 URL
         System.out.println("request.getRequestURL() = " + request.getRequestURL());
         //request-test
         System.out.println("request.getRequestURI() = " + request.getRequestURI());
@@ -32,5 +33,21 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.isSecure() = " + request.isSecure()); // https 사용 유무
         System.out.println("--- REQUEST-LINE - end ---");
         System.out.println();
+    }
+
+    //Header 모든 정보
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+        /* 과거에 사용한 방법
+         Enumeration<String> headerNames = request.getHeaderNames();
+         while (headerNames.hasMoreElements()) {
+         String headerName = headerNames.nextElement();
+         System.out.println(headerName + ": " + request.getHeader(headerName));
+         }
+        */
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> System.out.println(headerName + ": " + request.getHeader(headerName)));
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+
     }
 }
---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,54 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/* [목표 기능]
+* 1. 파라미터 전송 기능
+* http://localhost:8080/request-param?username=hello&age=20&username=hello22
+*
+*
+* */
+
+
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {super.service(request, response);
+        // 1. request(요청) 파라미터 모두 조회하는 방법
+        System.out.println("[전체 파라미터 조회] -  start");
+        // paramName = 파라미터 이름 꺼내기 Ex. age, name
+        // request.getParameter == 파라미터 이름에 해당하는 내용을 꺼내기 Ex. 김현빈, 23 등
+        // getParamter은 하나의 파라미터 이름에 대해서 첫 번째 단 하나의 값만 출력해준다.
+        // 따라서 중복일 경우에는 request.getParameterValues()를 사용해주어야한다.
+        // 하지만 주로 단일 파라미터를 사용한다.
+
+
+        request.getParameterNames().asIterator().forEachRemaining(
+                (paramName -> System.out.println(paramName+"="+request.getParameter(paramName))));
+        System.out.println("[전체 파라미터 조회] -  end");
+        System.out.println();
+
+        // 2. 단일 요청 파라미터 조회하는 방법
+        System.out.println("[단일 파라미터 조회] -  start");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = "+username + "age = "+ age);
+        System.out.println("[단일 파라미터 조회] -  end");
+        System.out.println();
+
+        // 3. 같은 파라미터 이름에 인풋 값이 복수인 경우 조회하기
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames = request.getParameterValues("username");
+        for (String name:usernames){
+            System.out.println("username = " + name);
+        }
+    }
+}
