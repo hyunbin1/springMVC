3695195873d62cbbef80ae5a691da79930819088
hyunbin1 <kimgusqls1@gmail.com>
2021-10-31 14:53:41 +0900

    21/10/31 "서블릿"으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/hello/servlet/web/MemberSaveServlet.java
@@ -0,0 +1,49 @@
+package hello.servlet.web;
+
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+        private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age")); // getParameter은 항상 string만 입력받기 때문에 숫자로 변환시켜주어야한다.
+
+        // 멤버 생성자에 이름, 나이를 입력한 후
+        Member member = new Member(username, age);
+        // 생성된 멤버를 레퍼지토리에 저장한다.
+        memberRepository.save(member);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        PrintWriter w = resp.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
