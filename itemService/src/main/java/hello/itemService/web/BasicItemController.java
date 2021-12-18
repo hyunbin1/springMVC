04ff2d6662e430e10449b199e746f6ed254dd145
hyunbin1 <kimgusqls1@gmail.com>
2021-12-18 18:50:08 +0900

    2021/12/18 상품 등록 폼 완성

--- itemService/src/main/java/hello/itemService/web/BasicItemController.java
+++ itemService/src/main/java/hello/itemService/web/BasicItemController.java
@@ -6,12 +6,11 @@ import hello.itemService.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.message.ExitMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.cglib.reflect.MethodDelegate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,14 +35,61 @@ public class BasicItemController {
         return "basic/items";
     }
 
-    //상품 상세 whghl
+    //상품 상세 조회
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
-
     }
+
+    // 상품 등록 폼 조회
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    //상품 등록
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model)
+    {
+        Item item =new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); // 자동 추가되기 때문에 생략 가능
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model){
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+
+        return "basic/item";
+    }
+
 
     // 테스트 용 초기 데이터 추가
     @PostConstruct
