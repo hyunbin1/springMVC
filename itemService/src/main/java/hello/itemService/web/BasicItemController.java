9013fe20d0766ded16c50ac94937a2320fe30a1b
hyunbin1 <kimgusqls1@gmail.com>
2021-12-18 22:22:43 +0900

    2021/12/18 상품 상세 페이지 중복 방지

--- itemService/src/main/java/hello/itemService/web/BasicItemController.java
+++ itemService/src/main/java/hello/itemService/web/BasicItemController.java
@@ -10,6 +10,7 @@ import org.springframework.cglib.reflect.MethodDelegate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -82,12 +83,29 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item){
         itemRepository.save(item);
 
         return "basic/item";
     }
+//    @PostMapping("/add")
+    public String addItemV5(Item item){
+        itemRepository.save(item);
+
+        return "redirect:/basic/items/"+item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
 
     //상품 수정 페이지 조회
     @GetMapping("/{itemId}/edit")
@@ -105,15 +123,6 @@ public class BasicItemController {
     }
 
     // 상품 수정 취소 기능은 추가하지 않아도 된다. - 처리되는 데이터가 없기 때문
-
-
-    
-    
-//    @PostMapping("/{itemId}/edit")
-//    public String edit(Item item){
-//
-//    }
-
 
 
     // 테스트 용 초기 데이터 추가
